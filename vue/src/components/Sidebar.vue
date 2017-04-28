caf4c0d9f91e93d95aa3a21619eb57b354f1162a
Youngbok Yoon <bok95@bokBook.local>
2017-04-28 14:09:40 +0900

    [APIWAY-40] Sidebar item 추가 및 디자인 수정

--- vue/src/components/Sidebar.vue
+++ vue/src/components/Sidebar.vue
@@ -2,8 +2,8 @@
   <div class="sidebar">
     <nav class="sidebar-nav">
       <ul class="nav">
-        <li class="nav-item">
-          <router-link :to="'/dashboard'" class="nav-link"><i class="icon-speedometer"></i> Dashboard <span class="badge badge-info">NEW</span></router-link>
+        <li class="nav-item mt-1 mb-1">
+          <router-link :to="'/dashboard'" class="nav-link"><i class="fa fa-tachometer"></i><span>Monitoring</span></router-link>
         </li>
       </ul>
     </nav>
@@ -22,8 +22,20 @@ export default {
 }
 </script>
 
-<style lang="css">
+<style lang="css" scoped>
   .nav-link {
     cursor:pointer;
+  }
+
+  .sidebar-nav .nav .nav-item i {
+    width: 100%;
+  }
+
+  .sidebar-nav .nav .nav-item span {
+    width: 100%;
+  }
+
+  nav ul .fa:before {
+    font-size: 24px !important;
   }
 </style>
