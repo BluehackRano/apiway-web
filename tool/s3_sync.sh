6485490feebb4baac2d986b2a33a3e3cf1e09769
김대섭 <daesubkim@gimdaeseob-ui-MacBook-Pro.local>
2017-07-10 14:06:46 +0900

    Changed the tool/s3_sync.sh file location

--- tool/s3_sync.sh
+++ tool/s3_sync.sh
@@ -2,5 +2,5 @@
 
 #s3cmd --exclude=".git/*" sync --recursive --cf-invalidate --cf-invalidate-default-index --default-mime-type="text/html" --guess-mime-type ~/amberstones.net/yellowButton/web/* s3://yellow-button-web
 
-TOP="/Users/bok95/bluehack/apiway/tower/tower-web-console"
+TOP="/Users/daesubkim/Desktop/ApiWay/Web/apiway-web"
 s3cmd sync --exclude=".git/*" --exclude="node_modules/*" --recursive --default-mime-type="text/html" --guess-mime-type $TOP/vue/dist/* s3://apiway.io -c ~/.s3cfg
