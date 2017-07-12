599bd5e7d9256577edfe461d08d7f0cf2632fe23
김대섭 <daesubkim@gimdaeseob-ui-MacBook-Pro.local>
2017-07-13 04:40:47 +0900

    Resolved APIWAY-225 : Added auth.js file for handling access-token

---
+++ vue/src/util/auth.js
@@ -0,0 +1,14 @@
+export function getToken () {
+  return localStorage.getItem('access-token')
+}
+
+export function setToken (aToken) {
+  return localStorage.setItem('access-token', aToken)
+}
+
+export function isAuthenticated () {
+  if (this.getToken()) {
+    return true
+  }
+  return false
+}
